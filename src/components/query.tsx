c948b95610b377ac7c013310eeae682fa6c1c2fb
wilsson <wilsonft83@gmail.com>
2018-02-07 19:02:52 -0500

    feat(router): organisando rutas

--- src/components/query.tsx
+++ src/components/query.tsx
@@ -4,11 +4,7 @@ export const Query = (props) => {
     console.log('Query>', props.match)
     return(
         <div id='query'>
-            {props.match.params.name}
+            query: {JSON.stringify(props.match.params)}
         </div>
     )
 };
-
-export const Topic = (props) => {
-    return <h1>{props.match.params.name}</h1>
-  }
--- src/entry.tsx
+++ src/entry.tsx
@@ -11,7 +11,6 @@ import { Home } from './components/home';
 import { About } from './components/about';
 import { Contact } from './components/contact';
 import { Query } from './components/query';
-import { Topic } from './components/query';
 import { NotFound } from './components/notfound';
 
 const Child = ({ match }) => (
@@ -19,29 +18,58 @@ const Child = ({ match }) => (
       <h3>ID: {match.params.id}</h3>
     </div>
   )
-const App = (props) => {
-    return(
-        <Router>
-            <div>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-                <li><Link to='/query'>Query</Link></li>
-            </ul>
 
-            <hr/>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path='/about' component={About}/>
-                <Route path='/contact' component={Contact}/>
-                <Route path='/query/:id' component={Query} />
-                <Route path="/topics/:name" component={Topic} />
-                <Route component={NotFound} />
-            </Switch>
-            </div>
-        </Router>
-    )
+interface AppProps {
+    router?: any;
+    history?: any;
+}
+
+interface AppState {
+    url?: string;
+}
+class App extends React.Component<AppProps, AppState> {
+    constructor(props){
+        super(props);
+        this.state = { url: '' };
+    }
+    onclick(e){
+        console.log('onclick', this.props.history);
+        this.props.router.push('/'+ this.state.url);
+    }
+
+    onchange(e){
+        console.log('this onchange', e.target.value);
+        this.setState((prevState) => {
+            return {
+                url: e.target
+            }
+        });
+    }
+    render(){
+        return(
+            <Router>
+                <div>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
+                    <li><Link to='/query'>Query</Link></li>
+                </ul>
+
+                <hr/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path='/about' component={About}/>
+                    <Route path='/contact' component={Contact}/>
+                    <Route path='/query/:foo/:id' component={Query} />
+                    <Route component={NotFound} />
+                </Switch>
+                <input type="text" onChange={this.onchange.bind(this)}/>
+                <button onClick={this.onclick.bind(this)}>change url</button>
+                </div>
+            </Router>
+        )
+    }
 };
 
 ReactDOM.render(
